2160edb9b05b5be80613a372c11575abf74d3a00
Tobias Klauser <tklauser@distanz.ch>
2019-01-09 11:11:56 +0100

    Add vendor ID for the bhyve hypervisor

--- cpuid.go
+++ cpuid.go
@@ -26,6 +26,7 @@ const (
 	MSVM // Microsoft Hyper-V or Windows Virtual PC
 	VMware
 	XenHVM
+	Bhyve
 )
 
 const (
@@ -527,7 +528,7 @@ func (c CPUInfo) LogicalCPU() int {
 // have many false negatives.
 func (c CPUInfo) VM() bool {
 	switch c.VendorID {
-	case MSVM, KVM, VMware, XenHVM:
+	case MSVM, KVM, VMware, XenHVM, Bhyve:
 		return true
 	}
 	return false
@@ -670,6 +671,7 @@ var vendorMapping = map[string]Vendor{
 	"Microsoft Hv": MSVM,
 	"VMwareVMware": VMware,
 	"XenVMMXenVMM": XenHVM,
+	"bhyve bhyve ": Bhyve,
 }
 
 func vendorID() Vendor {
