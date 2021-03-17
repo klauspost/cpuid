aa20eb5ca3860122f7a282b06922c84799c5de60
Klaus Post <klauspost@gmail.com>
2021-03-17 12:04:18 +0100

    Simplify and separate.

--- os_other_arm64.go
+++ os_other_arm64.go
@@ -7,5 +7,9 @@
 package cpuid
 
 func detectOS(c *CPUInfo) bool {
+	c.PhysicalCores = runtime.NumCPU()
+	// For now assuming 1 thread per core...
+	c.ThreadsPerCore = 1
+	c.LogicalCores = c.PhysicalCores
 	return false
 }
