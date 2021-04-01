klauspost/cpuid

commit 43f4c41326096a3d90b250136a41d7e0c46bbcd8

v2 refactor and API changes (#62)
Refactors the API to make it smaller and easier to maintain.

Adds ARM64 detection.

Many tweaks and fixes.

Fixes #23
Fixes #52
Fixes #63
Fixes #4 (a bit more)
Replaces #31

featureid_string.go
@@ -1,0 +1,161 @@
+// Code generated by "stringer -type=FeatureID,Vendor"; DO NOT EDIT.
+
+package cpuid
+
+import "strconv"
+
+func _() {
+	// An "invalid array index" compiler error signifies that the constant values have changed.
+	// Re-run the stringer command to generate them again.
+	var x [1]struct{}
+	_ = x[ADX-1]
+	_ = x[AESNI-2]
+	_ = x[AMD3DNOW-3]
+	_ = x[AMD3DNOWEXT-4]
+	_ = x[AMXBF16-5]
+	_ = x[AMXINT8-6]
+	_ = x[AMXTILE-7]
+	_ = x[AVX-8]
+	_ = x[AVX2-9]
+	_ = x[AVX512BF16-10]
+	_ = x[AVX512BITALG-11]
+	_ = x[AVX512BW-12]
+	_ = x[AVX512CD-13]
+	_ = x[AVX512DQ-14]
+	_ = x[AVX512ER-15]
+	_ = x[AVX512F-16]
+	_ = x[AVX512IFMA-17]
+	_ = x[AVX512PF-18]
+	_ = x[AVX512VBMI-19]
+	_ = x[AVX512VBMI2-20]
+	_ = x[AVX512VL-21]
+	_ = x[AVX512VNNI-22]
+	_ = x[AVX512VP2INTERSECT-23]
+	_ = x[AVX512VPOPCNTDQ-24]
+	_ = x[AVXSLOW-25]
+	_ = x[BMI1-26]
+	_ = x[BMI2-27]
+	_ = x[CLDEMOTE-28]
+	_ = x[CLMUL-29]
+	_ = x[CMOV-30]
+	_ = x[CX16-31]
+	_ = x[ENQCMD-32]
+	_ = x[ERMS-33]
+	_ = x[F16C-34]
+	_ = x[FMA3-35]
+	_ = x[FMA4-36]
+	_ = x[GFNI-37]
+	_ = x[HLE-38]
+	_ = x[HTT-39]
+	_ = x[HYPERVISOR-40]
+	_ = x[IBPB-41]
+	_ = x[IBS-42]
+	_ = x[IBSBRNTRGT-43]
+	_ = x[IBSFETCHSAM-44]
+	_ = x[IBSFFV-45]
+	_ = x[IBSOPCNT-46]
+	_ = x[IBSOPCNTEXT-47]
+	_ = x[IBSOPSAM-48]
+	_ = x[IBSRDWROPCNT-49]
+	_ = x[IBSRIPINVALIDCHK-50]
+	_ = x[LZCNT-51]
+	_ = x[MMX-52]
+	_ = x[MMXEXT-53]
+	_ = x[MOVDIR64B-54]
+	_ = x[MOVDIRI-55]
+	_ = x[MPX-56]
+	_ = x[NX-57]
+	_ = x[POPCNT-58]
+	_ = x[RDRAND-59]
+	_ = x[RDSEED-60]
+	_ = x[RDTSCP-61]
+	_ = x[RTM-62]
+	_ = x[SERIALIZE-63]
+	_ = x[SGX-64]
+	_ = x[SGXLC-65]
+	_ = x[SHA-66]
+	_ = x[SSE-67]
+	_ = x[SSE2-68]
+	_ = x[SSE3-69]
+	_ = x[SSE4-70]
+	_ = x[SSE42-71]
+	_ = x[SSE4A-72]
+	_ = x[SSSE3-73]
+	_ = x[STIBP-74]
+	_ = x[TBM-75]
+	_ = x[TSXLDTRK-76]
+	_ = x[VAES-77]
+	_ = x[VMX-78]
+	_ = x[VPCLMULQDQ-79]
+	_ = x[WAITPKG-80]
+	_ = x[WBNOINVD-81]
+	_ = x[XOP-82]
+	_ = x[AESARM-83]
+	_ = x[ARMCPUID-84]
+	_ = x[ASIMD-85]
+	_ = x[ASIMDDP-86]
+	_ = x[ASIMDHP-87]
+	_ = x[ASIMDRDM-88]
+	_ = x[ATOMICS-89]
+	_ = x[CRC32-90]
+	_ = x[DCPOP-91]
+	_ = x[EVTSTRM-92]
+	_ = x[FCMA-93]
+	_ = x[FP-94]
+	_ = x[FPHP-95]
+	_ = x[GPA-96]
+	_ = x[JSCVT-97]
+	_ = x[LRCPC-98]
+	_ = x[PMULL-99]
+	_ = x[SHA1-100]
+	_ = x[SHA2-101]
+	_ = x[SHA3-102]
+	_ = x[SHA512-103]
+	_ = x[SM3-104]
+	_ = x[SM4-105]
+	_ = x[SVE-106]
+	_ = x[lastID-107]
+	_ = x[firstID-0]
+}
+
+const _FeatureID_name = "firstIDADXAESNIAMD3DNOWAMD3DNOWEXTAMXBF16AMXINT8AMXTILEAVXAVX2AVX512BF16AVX512BITALGAVX512BWAVX512CDAVX512DQAVX512ERAVX512FAVX512IFMAAVX512PFAVX512VBMIAVX512VBMI2AVX512VLAVX512VNNIAVX512VP2INTERSECTAVX512VPOPCNTDQAVXSLOWBMI1BMI2CLDEMOTECLMULCMOVCX16ENQCMDERMSF16CFMA3FMA4GFNIHLEHTTHYPERVISORIBPBIBSIBSBRNTRGTIBSFETCHSAMIBSFFVIBSOPCNTIBSOPCNTEXTIBSOPSAMIBSRDWROPCNTIBSRIPINVALIDCHKLZCNTMMXMMXEXTMOVDIR64BMOVDIRIMPXNXPOPCNTRDRANDRDSEEDRDTSCPRTMSERIALIZESGXSGXLCSHASSESSE2SSE3SSE4SSE42SSE4ASSSE3STIBPTBMTSXLDTRKVAESVMXVPCLMULQDQWAITPKGWBNOINVDXOPAESARMARMCPUIDASIMDASIMDDPASIMDHPASIMDRDMATOMICSCRC32DCPOPEVTSTRMFCMAFPFPHPGPAJSCVTLRCPCPMULLSHA1SHA2SHA3SHA512SM3SM4SVElastID"
+
+var _FeatureID_index = [...]uint16{0, 7, 10, 15, 23, 34, 41, 48, 55, 58, 62, 72, 84, 92, 100, 108, 116, 123, 133, 141, 151, 162, 170, 180, 198, 213, 220, 224, 228, 236, 241, 245, 249, 255, 259, 263, 267, 271, 275, 278, 281, 291, 295, 298, 308, 319, 325, 333, 344, 352, 364, 380, 385, 388, 394, 403, 410, 413, 415, 421, 427, 433, 439, 442, 451, 454, 459, 462, 465, 469, 473, 477, 482, 487, 492, 497, 500, 508, 512, 515, 525, 532, 540, 543, 549, 557, 562, 569, 576, 584, 591, 596, 601, 608, 612, 614, 618, 621, 626, 631, 636, 640, 644, 648, 654, 657, 660, 663, 669}
+
+func (i FeatureID) String() string {
+	if i < 0 || i >= FeatureID(len(_FeatureID_index)-1) {
+		return "FeatureID(" + strconv.FormatInt(int64(i), 10) + ")"
+	}
+	return _FeatureID_name[_FeatureID_index[i]:_FeatureID_index[i+1]]
+}
+func _() {
+	// An "invalid array index" compiler error signifies that the constant values have changed.
+	// Re-run the stringer command to generate them again.
+	var x [1]struct{}
+	_ = x[VendorUnknown-0]
+	_ = x[Intel-1]
+	_ = x[AMD-2]
+	_ = x[VIA-3]
+	_ = x[Transmeta-4]
+	_ = x[NSC-5]
+	_ = x[KVM-6]
+	_ = x[MSVM-7]
+	_ = x[VMware-8]
+	_ = x[XenHVM-9]
+	_ = x[Bhyve-10]
+	_ = x[Hygon-11]
+	_ = x[SiS-12]
+	_ = x[RDC-13]
+	_ = x[lastVendor-14]
+}
+
+const _Vendor_name = "VendorUnknownIntelAMDVIATransmetaNSCKVMMSVMVMwareXenHVMBhyveHygonSiSRDClastVendor"
+
+var _Vendor_index = [...]uint8{0, 13, 18, 21, 24, 33, 36, 39, 43, 49, 55, 60, 65, 68, 71, 81}
+
+func (i Vendor) String() string {
+	if i < 0 || i >= Vendor(len(_Vendor_index)-1) {
+		return "Vendor(" + strconv.FormatInt(int64(i), 10) + ")"
+	}
+	return _Vendor_name[_Vendor_index[i]:_Vendor_index[i+1]]
+}

.travis.yml
@@ -21,17 +21,13 @@ script:
   - go test -race ./...
   - go test -tags=noasm ./...
 
-stages:
-  - gofmt
-  - test
-
 matrix:
   allow_failures:
     - go: 'master'
   fast_finish: true
   include:
     - stage: gofmt
-      go: 1.14.x
+      go: 1.15.x
       os: linux
       arch: amd64
       script:
@@ -40,8 +36,28 @@ matrix:
         - go install github.com/klauspost/asmfmt/cmd/asmfmt
         - diff <(asmfmt -d .) <(printf "")
     - stage: i386
-      go: 1.14.x
+      go: 1.15.x
       os: linux
       arch: amd64
       script:
         - GOOS=linux GOARCH=386 go test .
+    - stage: buildotherprev
+      go: 1.14.x
+      os: linux
+      arch: amd64
+      script:
+        - GOOS=darwin GOARCH=arm64 go build .
+        - GOOS=freebsd GOARCH=arm64 go build .
+        - GOOS=netbsd GOARCH=arm64 go build .
+        - GOOS=freebsd GOARCH=amd64 go build .
+        - GOOS=netbsd GOARCH=amd64 go build .
+    - stage: buildother
+      go: 1.15.x
+      os: linux
+      arch: amd64
+      script:
+        - GOOS=darwin GOARCH=arm64 go build .
+        - GOOS=freebsd GOARCH=arm64 go build .
+        - GOOS=netbsd GOARCH=arm64 go build .
+        - GOOS=freebsd GOARCH=amd64 go build .
+        - GOOS=netbsd GOARCH=amd64 go build .

cpuid_arm64.s
@@ -1,6 +1,6 @@
 // Copyright (c) 2015 Klaus Post, released under MIT License. See LICENSE file.
 
-//+build arm64,!gccgo
+//+build arm64,!gccgo,!noasm,!appengine
 
 // See https://www.kernel.org/doc/Documentation/arm64/cpu-feature-registers.txt
 

detect_arm64.go
@@ -4,6 +4,8 @@
 
 package cpuid
 
+import "runtime"
+
 func getMidr() (midr uint64)
 func getProcFeatures() (procFeatures uint64)
 func getInstAttributes() (instAttrReg0, instAttrReg1 uint64)
@@ -15,9 +17,16 @@ func initCPU() {
 	rdtscpAsm = func() (a, b, c, d uint32) { return 0, 0, 0, 0 }
 }
 
-func addInfo(c *CPUInfo) {
-	// ARM64 disabled for now.
-	if true {
+func addInfo(c *CPUInfo, safe bool) {
+	// Seems to be safe to assume on ARM64
+	c.CacheLine = 64
+	if detectOS(c) {
+		// We could detect values from OS, fine.
+		return
+	}
+
+	// ARM64 disabled since it may crash if interrupt is not intercepted by OS.
+	if safe && runtime.GOOS != "freebsd" {
 		return
 	}
 	// 	midr := getMidr()
@@ -68,25 +77,18 @@ func addInfo(c *CPUInfo) {
 	// | EL0                          | [3-0]   |    n    |
 	// x--------------------------------------------------x
 
-	var f ArmFlags
+	var f flagSet
 	// if procFeatures&(0xf<<48) != 0 {
 	// 	fmt.Println("DIT")
 	// }
-	if procFeatures&(0xf<<32) != 0 {
-		f |= SVE
-	}
+	f.setIf(procFeatures&(0xf<<32) != 0, SVE)
 	if procFeatures&(0xf<<20) != 15<<20 {
-		f |= ASIMD
-		if procFeatures&(0xf<<20) == 1<<20 {
-			// https://developer.arm.com/docs/ddi0595/b/aarch64-system-registers/id_aa64pfr0_el1
-			// 0b0001 --> As for 0b0000, and also includes support for half-precision floating-point arithmetic.
-			f |= FPHP
-			f |= ASIMDHP
-		}
+		f.set(ASIMD)
+		// https://developer.arm.com/docs/ddi0595/b/aarch64-system-registers/id_aa64pfr0_el1
+		// 0b0001 --> As for 0b0000, and also includes support for half-precision floating-point arithmetic.
+		f.setIf(procFeatures&(0xf<<20) == 1<<20, FPHP, ASIMDHP)
 	}
-	if procFeatures&(0xf<<16) != 0 {
-		f |= FP
-	}
+	f.setIf(procFeatures&(0xf<<16) != 0, FP)
 
 	instAttrReg0, instAttrReg1 := getInstAttributes()
 
@@ -127,46 +129,22 @@ func addInfo(c *CPUInfo) {
 	// if instAttrReg0&(0xf<<48) != 0 {
 	// 	fmt.Println("FHM")
 	// }
-	if instAttrReg0&(0xf<<44) != 0 {
-		f |= ASIMDDP
-	}
-	if instAttrReg0&(0xf<<40) != 0 {
-		f |= SM4
-	}
-	if instAttrReg0&(0xf<<36) != 0 {
-		f |= SM3
-	}
-	if instAttrReg0&(0xf<<32) != 0 {
-		f |= SHA3
-	}
-	if instAttrReg0&(0xf<<28) != 0 {
-		f |= ASIMDRDM
-	}
-	if instAttrReg0&(0xf<<20) != 0 {
-		f |= ATOMICS
-	}
-	if instAttrReg0&(0xf<<16) != 0 {
-		f |= CRC32
-	}
-	if instAttrReg0&(0xf<<12) != 0 {
-		f |= SHA2
-	}
-	if instAttrReg0&(0xf<<12) == 2<<12 {
-		// https://developer.arm.com/docs/ddi0595/b/aarch64-system-registers/id_aa64isar0_el1
-		// 0b0010 --> As 0b0001, plus SHA512H, SHA512H2, SHA512SU0, and SHA512SU1 instructions implemented.
-		f |= SHA512
-	}
-	if instAttrReg0&(0xf<<8) != 0 {
-		f |= SHA1
-	}
-	if instAttrReg0&(0xf<<4) != 0 {
-		f |= AES
-	}
-	if instAttrReg0&(0xf<<4) == 2<<4 {
-		// https://developer.arm.com/docs/ddi0595/b/aarch64-system-registers/id_aa64isar0_el1
-		// 0b0010 --> As for 0b0001, plus PMULL/PMULL2 instructions operating on 64-bit data quantities.
-		f |= PMULL
-	}
+	f.setIf(instAttrReg0&(0xf<<44) != 0, ASIMDDP)
+	f.setIf(instAttrReg0&(0xf<<40) != 0, SM4)
+	f.setIf(instAttrReg0&(0xf<<36) != 0, SM3)
+	f.setIf(instAttrReg0&(0xf<<32) != 0, SHA3)
+	f.setIf(instAttrReg0&(0xf<<28) != 0, ASIMDRDM)
+	f.setIf(instAttrReg0&(0xf<<20) != 0, ATOMICS)
+	f.setIf(instAttrReg0&(0xf<<16) != 0, CRC32)
+	f.setIf(instAttrReg0&(0xf<<12) != 0, SHA2)
+	// https://developer.arm.com/docs/ddi0595/b/aarch64-system-registers/id_aa64isar0_el1
+	// 0b0010 --> As 0b0001, plus SHA512H, SHA512H2, SHA512SU0, and SHA512SU1 instructions implemented.
+	f.setIf(instAttrReg0&(0xf<<12) == 2<<12, SHA512)
+	f.setIf(instAttrReg0&(0xf<<8) != 0, SHA1)
+	f.setIf(instAttrReg0&(0xf<<4) != 0, AESARM)
+	// https://developer.arm.com/docs/ddi0595/b/aarch64-system-registers/id_aa64isar0_el1
+	// 0b0010 --> As for 0b0001, plus PMULL/PMULL2 instructions operating on 64-bit data quantities.
+	f.setIf(instAttrReg0&(0xf<<4) == 2<<4, PMULL)
 
 	// https://developer.arm.com/docs/ddi0595/b/aarch64-system-registers/id_aa64isar1_el1
 	//
@@ -194,26 +172,18 @@ func addInfo(c *CPUInfo) {
 	// if instAttrReg1&(0xf<<28) != 0 {
 	// 	fmt.Println("GPI")
 	// }
-	if instAttrReg1&(0xf<<28) != 24 {
-		f |= GPA
-	}
-	if instAttrReg1&(0xf<<20) != 0 {
-		f |= LRCPC
-	}
-	if instAttrReg1&(0xf<<16) != 0 {
-		f |= FCMA
-	}
-	if instAttrReg1&(0xf<<12) != 0 {
-		f |= JSCVT
-	}
+	f.setIf(instAttrReg1&(0xf<<28) != 24, GPA)
+	f.setIf(instAttrReg1&(0xf<<20) != 0, LRCPC)
+	f.setIf(instAttrReg1&(0xf<<16) != 0, FCMA)
+	f.setIf(instAttrReg1&(0xf<<12) != 0, JSCVT)
 	// if instAttrReg1&(0xf<<8) != 0 {
 	// 	fmt.Println("API")
 	// }
 	// if instAttrReg1&(0xf<<4) != 0 {
 	// 	fmt.Println("APA")
 	// }
-	if instAttrReg1&(0xf<<0) != 0 {
-		f |= DCPOP
-	}
-	c.Arm = f
+	f.setIf(instAttrReg1&(0xf<<0) != 0, DCPOP)
+
+	// Store
+	c.featureSet.or(f)
 }

detect_ref.go
@@ -11,4 +11,4 @@ func initCPU() {
 	rdtscpAsm = func() (a, b, c, d uint32) { return 0, 0, 0, 0 }
 }
 
-func addInfo(info *CPUInfo) {}
+func addInfo(info *CPUInfo, safe bool) {}

detect_x86.go
@@ -16,16 +16,13 @@ func initCPU() {
 	rdtscpAsm = asmRdtscpAsm
 }
 
-func addInfo(c *CPUInfo) {
+func addInfo(c *CPUInfo, safe bool) {
 	c.maxFunc = maxFunctionID()
 	c.maxExFunc = maxExtendedFunction()
 	c.BrandName = brandName()
 	c.CacheLine = cacheLine()
 	c.Family, c.Model = familyModel()
 	c.featureSet = support()
-	if c.featureSet != nil {
-		c.Features = c.featureSet[0]
-	}
 	c.SGX = hasSGX(c.featureSet.inSet(SGX), c.featureSet.inSet(SGXLC))
 	c.ThreadsPerCore = threadsPerCore()
 	c.LogicalCores = logicalCores()

commit beb10149d972e9c108a4debc341e717981ca63cf

Fix SSE4 feature flag names
Fixes an API breakage caused by
43f4c41326096a3d90b250136a41d7e0c46bbcd8.

cpuid.go
@@ -32,8 +32,10 @@ const (
 	VIA
 	Transmeta
 	NSC
-	KVM  // Kernel-based Virtual Machine
-	MSVM // Microsoft Hyper-V or Windows Virtual PC
+	// Kernel-based Virtual Machine
+	KVM
+	// Microsoft Hyper-V or Windows Virtual PC
+	MSVM
 	VMware
 	XenHVM
 	Bhyve
@@ -57,7 +59,7 @@ const (
 	lastVendor
 )
 
-//go:generate stringer -type=FeatureID,Vendor
+//go:generate stringer -type=FeatureID,Vendor -linecomment
 
 // FeatureID is the ID of a specific cpu feature.
 type FeatureID int
@@ -66,115 +68,220 @@ const (
 	// Keep index -1 as unknown
 	UNKNOWN = -1
 
-	// Add features
-	ADX                FeatureID = iota // Intel ADX (Multi-Precision Add-Carry Instruction Extensions)
-	AESNI                               // Advanced Encryption Standard New Instructions
-	AMD3DNOW                            // AMD 3DNOW
-	AMD3DNOWEXT                         // AMD 3DNowExt
-	AMXBF16                             // Tile computational operations on BFLOAT16 numbers
-	AMXINT8                             // Tile computational operations on 8-bit integers
-	AMXTILE                             // Tile architecture
-	AVX                                 // AVX functions
-	AVX2                                // AVX2 functions
-	AVX512BF16                          // AVX-512 BFLOAT16 Instructions
-	AVX512BITALG                        // AVX-512 Bit Algorithms
-	AVX512BW                            // AVX-512 Byte and Word Instructions
-	AVX512CD                            // AVX-512 Conflict Detection Instructions
-	AVX512DQ                            // AVX-512 Doubleword and Quadword Instructions
-	AVX512ER                            // AVX-512 Exponential and Reciprocal Instructions
-	AVX512F                             // AVX-512 Foundation
-	AVX512IFMA                          // AVX-512 Integer Fused Multiply-Add Instructions
-	AVX512PF                            // AVX-512 Prefetch Instructions
-	AVX512VBMI                          // AVX-512 Vector Bit Manipulation Instructions
-	AVX512VBMI2                         // AVX-512 Vector Bit Manipulation Instructions, Version 2
-	AVX512VL                            // AVX-512 Vector Length Extensions
-	AVX512VNNI                          // AVX-512 Vector Neural Network Instructions
-	AVX512VP2INTERSECT                  // AVX-512 Intersect for D/Q
-	AVX512VPOPCNTDQ                     // AVX-512 Vector Population Count Doubleword and Quadword
-	AVXSLOW                             // Indicates the CPU performs 2 128 bit operations instead of one.
-	BMI1                                // Bit Manipulation Instruction Set 1
-	BMI2                                // Bit Manipulation Instruction Set 2
-	CLDEMOTE                            // Cache Line Demote
-	CLMUL                               // Carry-less Multiplication
-	CMOV                                // i686 CMOV
-	CX16                                // CMPXCHG16B Instruction
-	ENQCMD                              // Enqueue Command
-	ERMS                                // Enhanced REP MOVSB/STOSB
-	F16C                                // Half-precision floating-point conversion
-	FMA3                                // Intel FMA 3. Does not imply AVX.
-	FMA4                                // Bulldozer FMA4 functions
-	GFNI                                // Galois Field New Instructions
-	HLE                                 // Hardware Lock Elision
-	HTT                                 // Hyperthreading (enabled)
-	HYPERVISOR                          // This bit has been reserved by Intel & AMD for use by hypervisors
-	IBPB                                // Indirect Branch Restricted Speculation (IBRS) and Indirect Branch Predictor Barrier (IBPB)
-	IBS                                 // Instruction Based Sampling (AMD)
-	IBSBRNTRGT                          // Instruction Based Sampling Feature (AMD)
-	IBSFETCHSAM                         // Instruction Based Sampling Feature (AMD)
-	IBSFFV                              // Instruction Based Sampling Feature (AMD)
-	IBSOPCNT                            // Instruction Based Sampling Feature (AMD)
-	IBSOPCNTEXT                         // Instruction Based Sampling Feature (AMD)
-	IBSOPSAM                            // Instruction Based Sampling Feature (AMD)
-	IBSRDWROPCNT                        // Instruction Based Sampling Feature (AMD)
-	IBSRIPINVALIDCHK                    // Instruction Based Sampling Feature (AMD)
-	LZCNT                               // LZCNT instruction
-	MMX                                 // standard MMX
-	MMXEXT                              // SSE integer functions or AMD MMX ext
-	MOVDIR64B                           // Move 64 Bytes as Direct Store
-	MOVDIRI                             // Move Doubleword as Direct Store
-	MPX                                 // Intel MPX (Memory Protection Extensions)
-	NX                                  // NX (No-Execute) bit
-	POPCNT                              // POPCNT instruction
-	RDRAND                              // RDRAND instruction is available
-	RDSEED                              // RDSEED instruction is available
-	RDTSCP                              // RDTSCP Instruction
-	RTM                                 // Restricted Transactional Memory
-	SERIALIZE                           // Serialize Instruction Execution
-	SGX                                 // Software Guard Extensions
-	SGXLC                               // Software Guard Extensions Launch Control
-	SHA                                 // Intel SHA Extensions
-	SSE                                 // SSE functions
-	SSE2                                // P4 SSE functions
-	SSE3                                // Prescott SSE3 functions
-	SSE4                                // Penryn SSE4.1 functions
-	SSE42                               // Nehalem SSE4.2 functions
-	SSE4A                               // AMD Barcelona microarchitecture SSE4a instructions
-	SSSE3                               // Conroe SSSE3 functions
-	STIBP                               // Single Thread Indirect Branch Predictors
-	TBM                                 // AMD Trailing Bit Manipulation
-	TSXLDTRK                            // Intel TSX Suspend Load Address Tracking
-	VAES                                // Vector AES
-	VMX                                 // Virtual Machine Extensions
-	VPCLMULQDQ                          // Carry-Less Multiplication Quadword
-	WAITPKG                             // TPAUSE, UMONITOR, UMWAIT
-	WBNOINVD                            // Write Back and Do Not Invalidate Cache
-	XOP                                 // Bulldozer XOP functions
+	// Intel ADX (Multi-Precision Add-Carry Instruction Extensions)
+	ADX FeatureID = iota
+	// Advanced Encryption Standard New Instructions
+	AESNI
+	// AMD 3DNOW
+	AMD3DNOW
+	// AMD 3DNowExt
+	AMD3DNOWEXT
+	// Tile computational operations on BFLOAT16 numbers
+	AMXBF16
+	// Tile computational operations on 8-bit integers
+	AMXINT8
+	// Tile architecture
+	AMXTILE
+	// AVX functions
+	AVX
+	// AVX2 functions
+	AVX2
+	// AVX-512 BFLOAT16 Instructions
+	AVX512BF16
+	// AVX-512 Bit Algorithms
+	AVX512BITALG
+	// AVX-512 Byte and Word Instructions
+	AVX512BW
+	// AVX-512 Conflict Detection Instructions
+	AVX512CD
+	// AVX-512 Doubleword and Quadword Instructions
+	AVX512DQ
+	// AVX-512 Exponential and Reciprocal Instructions
+	AVX512ER
+	// AVX-512 Foundation
+	AVX512F
+	// AVX-512 Integer Fused Multiply-Add Instructions
+	AVX512IFMA
+	// AVX-512 Prefetch Instructions
+	AVX512PF
+	// AVX-512 Vector Bit Manipulation Instructions
+	AVX512VBMI
+	// AVX-512 Vector Bit Manipulation Instructions, Version 2
+	AVX512VBMI2
+	// AVX-512 Vector Length Extensions
+	AVX512VL
+	// AVX-512 Vector Neural Network Instructions
+	AVX512VNNI
+	// AVX-512 Intersect for D/Q
+	AVX512VP2INTERSECT
+	// AVX-512 Vector Population Count Doubleword and Quadword
+	AVX512VPOPCNTDQ
+	// Indicates the CPU performs 2 128 bit operations instead of one.
+	AVXSLOW
+	// Bit Manipulation Instruction Set 1
+	BMI1
+	// Bit Manipulation Instruction Set 2
+	BMI2
+	// Cache Line Demote
+	CLDEMOTE
+	// Carry-less Multiplication
+	CLMUL
+	// i686 CMOV
+	CMOV
+	// CMPXCHG16B Instruction
+	CX16
+	// Enqueue Command
+	ENQCMD
+	// Enhanced REP MOVSB/STOSB
+	ERMS
+	// Half-precision floating-point conversion
+	F16C
+	// Intel FMA 3. Does not imply AVX.
+	FMA3
+	// Bulldozer FMA4 functions
+	FMA4
+	// Galois Field New Instructions
+	GFNI
+	// Hardware Lock Elision
+	HLE
+	// Hyperthreading (enabled)
+	HTT
+	// This bit has been reserved by Intel & AMD for use by hypervisors
+	HYPERVISOR
+	// Indirect Branch Restricted Speculation (IBRS) and Indirect Branch Predictor Barrier (IBPB)
+	IBPB
+	// Instruction Based Sampling (AMD)
+	IBS
+	// Instruction Based Sampling Feature (AMD)
+	IBSBRNTRGT
+	// Instruction Based Sampling Feature (AMD)
+	IBSFETCHSAM
+	// Instruction Based Sampling Feature (AMD)
+	IBSFFV
+	// Instruction Based Sampling Feature (AMD)
+	IBSOPCNT
+	// Instruction Based Sampling Feature (AMD)
+	IBSOPCNTEXT
+	// Instruction Based Sampling Feature (AMD)
+	IBSOPSAM
+	// Instruction Based Sampling Feature (AMD)
+	IBSRDWROPCNT
+	// Instruction Based Sampling Feature (AMD)
+	IBSRIPINVALIDCHK
+	// LZCNT instruction
+	LZCNT
+	// standard MMX
+	MMX
+	// SSE integer functions or AMD MMX ext
+	MMXEXT
+	// Move 64 Bytes as Direct Store
+	MOVDIR64B
+	// Move Doubleword as Direct Store
+	MOVDIRI
+	// Intel MPX (Memory Protection Extensions)
+	MPX
+	// NX (No-Execute) bit
+	NX
+	// POPCNT instruction
+	POPCNT
+	// RDRAND instruction is available
+	RDRAND
+	// RDSEED instruction is available
+	RDSEED
+	// RDTSCP Instruction
+	RDTSCP
+	// Restricted Transactional Memory
+	RTM
+	// Serialize Instruction Execution
+	SERIALIZE
+	// Software Guard Extensions
+	SGX
+	// Software Guard Extensions Launch Control
+	SGXLC
+	// Intel SHA Extensions
+	SHA
+	// SSE functions
+	SSE
+	// P4 SSE functions
+	SSE2
+	// Prescott SSE3 functions
+	SSE3
+	// SSE4.1 Penryn SSE4.1 functions
+	SSE4 // SSE4.1
+	// Nehalem SSE4.2 functions
+	SSE42 // SSE4.2
+	// AMD Barcelona microarchitecture SSE4a instructions
+	SSE4A
+	// Conroe SSSE3 functions
+	SSSE3
+	// Single Thread Indirect Branch Predictors
+	STIBP
+	// AMD Trailing Bit Manipulation
+	TBM
+	// Intel TSX Suspend Load Address Tracking
+	TSXLDTRK
+	// Vector AES
+	VAES
+	// Virtual Machine Extensions
+	VMX
+	// Carry-Less Multiplication Quadword
+	VPCLMULQDQ
+	// TPAUSE, UMONITOR, UMWAIT
+	WAITPKG
+	// Write Back and Do Not Invalidate Cache
+	WBNOINVD
+	// Bulldozer XOP functions
+	XOP
 
 	// ARM features:
-	AESARM   // AES instructions
-	ARMCPUID // Some CPU ID registers readable at user-level
-	ASIMD    // Advanced SIMD
-	ASIMDDP  // SIMD Dot Product
-	ASIMDHP  // Advanced SIMD half-precision floating point
-	ASIMDRDM // Rounding Double Multiply Accumulate/Subtract (SQRDMLAH/SQRDMLSH)
-	ATOMICS  // Large System Extensions (LSE)
-	CRC32    // CRC32/CRC32C instructions
-	DCPOP    // Data cache clean to Point of Persistence (DC CVAP)
-	EVTSTRM  // Generic timer
-	FCMA     // Floatin point complex number addition and multiplication
-	FP       // Single-precision and double-precision floating point
-	FPHP     // Half-precision floating point
-	GPA      // Generic Pointer Authentication
-	JSCVT    // Javascript-style double->int convert (FJCVTZS)
-	LRCPC    // Weaker release consistency (LDAPR, etc)
-	PMULL    // Polynomial Multiply instructions (PMULL/PMULL2)
-	SHA1     // SHA-1 instructions (SHA1C, etc)
-	SHA2     // SHA-2 instructions (SHA256H, etc)
-	SHA3     // SHA-3 instructions (EOR3, RAXI, XAR, BCAX)
-	SHA512   // SHA512 instructions
-	SM3      // SM3 instructions
-	SM4      // SM4 instructions
-	SVE      // Scalable Vector Extension
+	// AES instructions
+	AESARM
+	// Some CPU ID registers readable at user-level
+	ARMCPUID
+	// Advanced SIMD
+	ASIMD
+	// SIMD Dot Product
+	ASIMDDP
+	// Advanced SIMD half-precision floating point
+	ASIMDHP
+	// Rounding Double Multiply Accumulate/Subtract (SQRDMLAH/SQRDMLSH)
+	ASIMDRDM
+	// Large System Extensions (LSE)
+	ATOMICS
+	// CRC32/CRC32C instructions
+	CRC32
+	// Data cache clean to Point of Persistence (DC CVAP)
+	DCPOP
+	// Generic timer
+	EVTSTRM
+	// Floatin point complex number addition and multiplication
+	FCMA
+	// Single-precision and double-precision floating point
+	FP
+	// Half-precision floating point
+	FPHP
+	// Generic Pointer Authentication
+	GPA
+	// Javascript-style double->int convert (FJCVTZS)
+	JSCVT
+	// Weaker release consistency (LDAPR, etc)
+	LRCPC
+	// Polynomial Multiply instructions (PMULL/PMULL2)
+	PMULL
+	// SHA-1 instructions (SHA1C, etc)
+	SHA1
+	// SHA-2 instructions (SHA256H, etc)
+	SHA2
+	// SHA-3 instructions (EOR3, RAXI, XAR, BCAX)
+	SHA3
+	// SHA512 instructions
+	SHA512
+	// SM3 instructions
+	SM3
+	// SM4 instructions
+	SM4
+	// Scalable Vector Extension
+	SVE
 
 	// Keep it last. It automatically defines the size of []flagSet
 	lastID

featureid_string.go
@@ -1,4 +1,4 @@
-// Code generated by "stringer -type=FeatureID,Vendor"; DO NOT EDIT.
+// Code generated by "stringer -type=FeatureID,Vendor -linecomment"; DO NOT EDIT.
 
 package cpuid
 
@@ -118,9 +118,9 @@ func _() {
 	_ = x[firstID-0]
 }
 
-const _FeatureID_name = "firstIDADXAESNIAMD3DNOWAMD3DNOWEXTAMXBF16AMXINT8AMXTILEAVXAVX2AVX512BF16AVX512BITALGAVX512BWAVX512CDAVX512DQAVX512ERAVX512FAVX512IFMAAVX512PFAVX512VBMIAVX512VBMI2AVX512VLAVX512VNNIAVX512VP2INTERSECTAVX512VPOPCNTDQAVXSLOWBMI1BMI2CLDEMOTECLMULCMOVCX16ENQCMDERMSF16CFMA3FMA4GFNIHLEHTTHYPERVISORIBPBIBSIBSBRNTRGTIBSFETCHSAMIBSFFVIBSOPCNTIBSOPCNTEXTIBSOPSAMIBSRDWROPCNTIBSRIPINVALIDCHKLZCNTMMXMMXEXTMOVDIR64BMOVDIRIMPXNXPOPCNTRDRANDRDSEEDRDTSCPRTMSERIALIZESGXSGXLCSHASSESSE2SSE3SSE4SSE42SSE4ASSSE3STIBPTBMTSXLDTRKVAESVMXVPCLMULQDQWAITPKGWBNOINVDXOPAESARMARMCPUIDASIMDASIMDDPASIMDHPASIMDRDMATOMICSCRC32DCPOPEVTSTRMFCMAFPFPHPGPAJSCVTLRCPCPMULLSHA1SHA2SHA3SHA512SM3SM4SVElastID"
+const _FeatureID_name = "firstIDADXAESNIAMD3DNOWAMD3DNOWEXTAMXBF16AMXINT8AMXTILEAVXAVX2AVX512BF16AVX512BITALGAVX512BWAVX512CDAVX512DQAVX512ERAVX512FAVX512IFMAAVX512PFAVX512VBMIAVX512VBMI2AVX512VLAVX512VNNIAVX512VP2INTERSECTAVX512VPOPCNTDQAVXSLOWBMI1BMI2CLDEMOTECLMULCMOVCX16ENQCMDERMSF16CFMA3FMA4GFNIHLEHTTHYPERVISORIBPBIBSIBSBRNTRGTIBSFETCHSAMIBSFFVIBSOPCNTIBSOPCNTEXTIBSOPSAMIBSRDWROPCNTIBSRIPINVALIDCHKLZCNTMMXMMXEXTMOVDIR64BMOVDIRIMPXNXPOPCNTRDRANDRDSEEDRDTSCPRTMSERIALIZESGXSGXLCSHASSESSE2SSE3SSE4.1SSE4.2SSE4ASSSE3STIBPTBMTSXLDTRKVAESVMXVPCLMULQDQWAITPKGWBNOINVDXOPAESARMARMCPUIDASIMDASIMDDPASIMDHPASIMDRDMATOMICSCRC32DCPOPEVTSTRMFCMAFPFPHPGPAJSCVTLRCPCPMULLSHA1SHA2SHA3SHA512SM3SM4SVElastID"
 
-var _FeatureID_index = [...]uint16{0, 7, 10, 15, 23, 34, 41, 48, 55, 58, 62, 72, 84, 92, 100, 108, 116, 123, 133, 141, 151, 162, 170, 180, 198, 213, 220, 224, 228, 236, 241, 245, 249, 255, 259, 263, 267, 271, 275, 278, 281, 291, 295, 298, 308, 319, 325, 333, 344, 352, 364, 380, 385, 388, 394, 403, 410, 413, 415, 421, 427, 433, 439, 442, 451, 454, 459, 462, 465, 469, 473, 477, 482, 487, 492, 497, 500, 508, 512, 515, 525, 532, 540, 543, 549, 557, 562, 569, 576, 584, 591, 596, 601, 608, 612, 614, 618, 621, 626, 631, 636, 640, 644, 648, 654, 657, 660, 663, 669}
+var _FeatureID_index = [...]uint16{0, 7, 10, 15, 23, 34, 41, 48, 55, 58, 62, 72, 84, 92, 100, 108, 116, 123, 133, 141, 151, 162, 170, 180, 198, 213, 220, 224, 228, 236, 241, 245, 249, 255, 259, 263, 267, 271, 275, 278, 281, 291, 295, 298, 308, 319, 325, 333, 344, 352, 364, 380, 385, 388, 394, 403, 410, 413, 415, 421, 427, 433, 439, 442, 451, 454, 459, 462, 465, 469, 473, 479, 485, 490, 495, 500, 503, 511, 515, 518, 528, 535, 543, 546, 552, 560, 565, 572, 579, 587, 594, 599, 604, 611, 615, 617, 621, 624, 629, 634, 639, 643, 647, 651, 657, 660, 663, 666, 672}
 
 func (i FeatureID) String() string {
 	if i < 0 || i >= FeatureID(len(_FeatureID_index)-1) {