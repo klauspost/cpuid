e6371685422577adc679b48b292447e2bf0fff03
Mostyn Bramley-Moore <mostyn@antipode.se>
2021-09-19 12:54:47 +0200

    Fix some lint warnings (#90)
    * Don't use fmt.Sprintf if we don't use any formatting
    * append multiple elements instead of calling append multiple times
    * Comment out a dead if branch
    * Remove superfluous return

--- cmd/cpuid/main.go
+++ cmd/cpuid/main.go
@@ -40,7 +40,7 @@ func main() {
 	fmt.Println("Threads Per Core:", cpuid.CPU.ThreadsPerCore)
 	fmt.Println("Logical Cores:", cpuid.CPU.LogicalCores)
 	fmt.Println("CPU Family", cpuid.CPU.Family, "Model:", cpuid.CPU.Model)
-	fmt.Println("Features:", fmt.Sprintf(strings.Join(cpuid.CPU.FeatureSet(), ",")))
+	fmt.Println("Features:", strings.Join(cpuid.CPU.FeatureSet(), ","))
 	fmt.Println("Cacheline bytes:", cpuid.CPU.CacheLine)
 	fmt.Println("L1 Instruction Cache:", cpuid.CPU.Cache.L1I, "bytes")
 	fmt.Println("L1 Data Cache:", cpuid.CPU.Cache.L1D, "bytes")
